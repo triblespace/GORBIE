2ef3864c2b4a2f8248871ef01ee6f6037f7e1c2b
⚫️ <jp@bultmann.eu>
2025-03-11 23:12:17 +0100

    add dataframe to widgets; improve load function names

--- src/widgets.rs
+++ src/widgets.rs
@@ -31,7 +31,7 @@ impl<T> std::fmt::Debug for LoadState<T> {
     }
 }
 
-pub fn button_spawn<'a, T: Send + 'static>(
+pub fn load_button<'a, T: Send + 'static>(
     ui: &mut egui::Ui,
     value: &'a mut LoadState<T>,
     label_init: &str,
@@ -62,7 +62,7 @@ pub fn button_spawn<'a, T: Send + 'static>(
     }
 }
 
-pub fn auto_spawn<'a, T: Send + 'static>(
+pub fn load_auto<'a, T: Send + 'static>(
     ui: &mut egui::Ui,
     value: &'a mut LoadState<T>,
     action: impl FnMut() -> T + Send + 'static,
@@ -90,4 +90,39 @@ pub fn auto_spawn<'a, T: Send + 'static>(
         LoadState::Ready(inner) => return Some(inner),
     }
     None
+}
+
+
+use egui_extras::{TableBuilder, Column};
+use polars::prelude::DataFrame;
+
+pub fn dataframe(ui: &mut egui::Ui, df: &DataFrame) {
+    let nr_cols = df.width();
+    let nr_rows = df.height();
+    let cols = &df.get_column_names();
+
+    TableBuilder::new(ui)
+        .columns(Column::remainder(), nr_cols)
+        .striped(true)
+        .header(30.0, |mut header| {
+            for head in cols {
+                header.col(|ui| {
+                    ui.label(egui::RichText::new(format!("{}", head)).heading().size(16.0));
+                });
+            }
+        })
+        .body(|body| {
+            body.rows(20.0, nr_rows, |mut row| {
+                for col in cols {
+                    let row_index = row.index();
+                    row.col(|ui| {
+                        if let Ok(column) = &df.column(col) {
+                            if let Ok(value) = column.get(row_index) {
+                                ui.label(format!("{}", value));
+                            }
+                        }
+                    });
+                }
+            });
+        });
 }
