509b5b9c699af2230a82cd866ce9311dd5acd50a
⚫️ <jp@bultmann.eu>
2025-03-18 10:37:26 +0100

    cleanup styling; add dark theme

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
+//! ## Working with mutable/non-cloneable things.
+//! Sometimes when working with existing code, libraries or even std things like
+//! files, can introduce an impedance mismatch with the reactive data-flow model.
+//! Often it is enough to wrap the object in question into another layer of `Arc`s
+//! and `RWLock`s in addition to what Gorby already does with it's `CardState`.
+//!
+//! - This is also why we compare explicit generations instead of return values,
+//! to broaden the range of types that can be used with `derive!`. -
+//! 
+//! But sometimes that isn't enough, e.g. when you want to display some application
+//! global state. This is why `state!` and `view!` are carefully designed to not
+//! rely on the dataflow mechanisms introduced by `derive`. Instead they can be
+//! used, like any other mutable rust type, modulo the `CardState` wrapper.
+//! 
+
 #![allow(non_snake_case)]
 
 pub mod cards;
@@ -9,7 +24,6 @@ pub use dataflow::*;
 use crate::egui::{FontData, FontDefinitions, FontFamily, FontId, TextStyle};
 use ctrlc;
 use eframe::egui::{self};
-use std::collections::BTreeMap;
 use tribles::prelude::*;
 
 /// A notebook is a collection of cards.
@@ -17,6 +31,140 @@ use tribles::prelude::*;
 /// Cards can be stateless, stateful, or reactively derived from other cards.
 pub struct Notebook {
     pub cards: Vec<(Id, Box<dyn Card>)>,
+}
+
+pub fn gorbie_fonts() -> FontDefinitions {
+    let mut fonts = FontDefinitions::default();
+    fonts.font_data.insert(
+        "lora".to_owned(),
+        std::sync::Arc::new(FontData::from_static(include_bytes!(
+            "../assets/fonts/Lora/Lora-VariableFont_wght.ttf"
+        ))),
+    );
+    fonts.font_data.insert("atkinson".to_owned(),
+        std::sync::Arc::new(
+            FontData::from_static(include_bytes!("../assets/fonts/Atkinson_Hyperlegible_Next/AtkinsonHyperlegibleNext-VariableFont_wght.ttf"))
+        )
+        );
+    fonts.font_data.insert(
+        "roboto_mono".to_owned(),
+        std::sync::Arc::new(FontData::from_static(include_bytes!(
+            "../assets/fonts/Roboto_Mono/RobotoMono-VariableFont_wght.ttf"
+        ))),
+    );
+    fonts
+        .families
+        .get_mut(&FontFamily::Proportional)
+        .unwrap()
+        .insert(0, "atkinson".to_owned());
+    fonts
+        .families
+        .get_mut(&FontFamily::Monospace)
+        .unwrap()
+        .insert(0, "roboto_mono".to_owned());
+
+    fonts
+        .families
+        .insert(FontFamily::Name("lora".into()), vec!["lora".into()]);
+    fonts
+        .families
+        .insert(FontFamily::Name("atkinson".into()), vec!["atkinson".into()]);
+    fonts.families.insert(
+        FontFamily::Name("roboto_mono".into()),
+        vec!["roboto_mono".into()],
+    );
+
+    fonts
+}
+
+pub fn gorbie_theme_light() -> egui::Style {
+    let mut style = egui::Style::default();
+    style.text_styles = [
+        (
+            TextStyle::Heading,
+            FontId::new(32.0, FontFamily::Name("lora".into())),
+        ),
+        (TextStyle::Body, FontId::new(16.0, FontFamily::Proportional)),
+        (
+            TextStyle::Monospace,
+            FontId::new(16.0, FontFamily::Monospace),
+        ),
+        (
+            TextStyle::Button,
+            FontId::new(16.0, FontFamily::Proportional),
+        ),
+        (
+            TextStyle::Small,
+            FontId::new(12.0, FontFamily::Proportional),
+        ),
+    ]
+    .into();
+
+    // Base color: #130496
+    // Fade to white:
+    // #130496 #5032a8 #7858ba #9b80cc #bda9dd #ded3ee #ffffff
+    // Fade to black:
+    // #130496 #1a087b #1c0b62 #1a0c4a #170b32 #12051d #000000
+    style.visuals.window_fill = egui::Color32::from_hex("#ffffff").unwrap();
+    style.visuals.panel_fill = egui::Color32::from_hex("#ffffff").unwrap();
+    style.visuals.faint_bg_color = egui::Color32::from_hex("#ded3ee").unwrap();
+    style.visuals.extreme_bg_color = egui::Color32::from_hex("#9b80cc").unwrap();
+    style.visuals.code_bg_color = egui::Color32::from_hex("#ded3ee").unwrap();
+    style.visuals.selection.bg_fill = egui::Color32::from_hex("#bda9dd").unwrap();
+    style.visuals.hyperlink_color = egui::Color32::from_hex("#130496").unwrap();
+    style.visuals.warn_fg_color = egui::Color32::from_hex("#1a087b").unwrap();
+    style.visuals.error_fg_color = egui::Color32::from_hex("#1c0b62").unwrap();
+    style.visuals.widgets.active.fg_stroke.color =
+        egui::Color32::from_hex("#170b32").unwrap();
+    style.visuals.override_text_color =
+        Some(egui::Color32::from_hex("#12051d").unwrap());
+
+    style
+}
+
+pub fn gorbie_theme_dark() -> egui::Style {
+    let mut style = egui::Style::default();
+    style.text_styles = [
+        (
+            TextStyle::Heading,
+            FontId::new(32.0, FontFamily::Name("lora".into())),
+        ),
+        (TextStyle::Body, FontId::new(16.0, FontFamily::Proportional)),
+        (
+            TextStyle::Monospace,
+            FontId::new(16.0, FontFamily::Monospace),
+        ),
+        (
+            TextStyle::Button,
+            FontId::new(16.0, FontFamily::Proportional),
+        ),
+        (
+            TextStyle::Small,
+            FontId::new(12.0, FontFamily::Proportional),
+        ),
+    ]
+    .into();
+
+    // Base color: #130496
+    // Fade to white:
+    // #130496 #5032a8 #7858ba #9b80cc #bda9dd #ded3ee #ffffff
+    // Fade to black:
+    // #130496 #1a087b #1c0b62 #1a0c4a #170b32 #12051d #000000
+    style.visuals.window_fill = egui::Color32::from_hex("#000000").unwrap();
+    style.visuals.panel_fill = egui::Color32::from_hex("#000000").unwrap();
+    style.visuals.faint_bg_color = egui::Color32::from_hex("#12051d").unwrap();
+    style.visuals.extreme_bg_color = egui::Color32::from_hex("#1a0c4a").unwrap();
+    style.visuals.code_bg_color = egui::Color32::from_hex("#12051d").unwrap();
+    style.visuals.selection.bg_fill = egui::Color32::from_hex("#170b32").unwrap();
+    style.visuals.hyperlink_color = egui::Color32::from_hex("#130496").unwrap();
+    style.visuals.warn_fg_color = egui::Color32::from_hex("#5032a8").unwrap();
+    style.visuals.error_fg_color = egui::Color32::from_hex("#7858ba").unwrap();
+    style.visuals.widgets.active.fg_stroke.color =
+        egui::Color32::from_hex("#bda9dd").unwrap();
+    style.visuals.override_text_color =
+        Some(egui::Color32::from_hex("#ded3ee").unwrap());
+
+    style
 }
 
 impl Notebook {
@@ -40,91 +188,9 @@ impl Notebook {
                 ctrlc::set_handler(move || ctx.send_viewport_cmd(egui::ViewportCommand::Close))
                     .expect("failed to set exit signal handler");
 
-                let mut fonts = FontDefinitions::default();
-                fonts.font_data.insert(
-                    "lora".to_owned(),
-                    std::sync::Arc::new(FontData::from_static(include_bytes!(
-                        "../assets/fonts/Lora/Lora-VariableFont_wght.ttf"
-                    ))),
-                );
-                fonts.font_data.insert("atkinson".to_owned(),
-                    std::sync::Arc::new(
-                        FontData::from_static(include_bytes!("../assets/fonts/Atkinson_Hyperlegible_Next/AtkinsonHyperlegibleNext-VariableFont_wght.ttf"))
-                    )
-                    );
-                fonts.font_data.insert(
-                    "roboto_mono".to_owned(),
-                    std::sync::Arc::new(FontData::from_static(include_bytes!(
-                        "../assets/fonts/Roboto_Mono/RobotoMono-VariableFont_wght.ttf"
-                    ))),
-                );
-                fonts
-                    .families
-                    .get_mut(&FontFamily::Proportional)
-                    .unwrap()
-                    .insert(0, "atkinson".to_owned());
-                fonts
-                    .families
-                    .get_mut(&FontFamily::Monospace)
-                    .unwrap()
-                    .insert(0, "roboto_mono".to_owned());
-
-                fonts
-                    .families
-                    .insert(FontFamily::Name("lora".into()), vec!["lora".into()]);
-                fonts
-                    .families
-                    .insert(FontFamily::Name("atkinson".into()), vec!["atkinson".into()]);
-                fonts.families.insert(
-                    FontFamily::Name("roboto_mono".into()),
-                    vec!["roboto_mono".into()],
-                );
-
-                cc.egui_ctx.set_fonts(fonts);
-
-                let text_styles: BTreeMap<_, _> = [
-                    (
-                        TextStyle::Heading,
-                        FontId::new(32.0, FontFamily::Name("lora".into())),
-                    ),
-                    (TextStyle::Body, FontId::new(16.0, FontFamily::Proportional)),
-                    (
-                        TextStyle::Monospace,
-                        FontId::new(16.0, FontFamily::Monospace),
-                    ),
-                    (
-                        TextStyle::Button,
-                        FontId::new(16.0, FontFamily::Proportional),
-                    ),
-                    (
-                        TextStyle::Small,
-                        FontId::new(12.0, FontFamily::Proportional),
-                    ),
-                ]
-                .into();
-
-                cc.egui_ctx.all_styles_mut(move |style| {
-                    style.text_styles = text_styles.clone();
-
-                    // Base color: #130496
-                    // Fade to white:
-                    // #130496 #5032a8 #7858ba #9b80cc #bda9dd #ded3ee #ffffff
-                    // Fade to black:
-                    // #130496 #1a087b #1c0b62 #1a0c4a #170b32 #12051d #000000
-                    style.visuals.window_fill = egui::Color32::from_hex("#ffffff").unwrap();
-                    style.visuals.panel_fill = egui::Color32::from_hex("#ffffff").unwrap();
-                    style.visuals.faint_bg_color = egui::Color32::from_hex("#ded3ee").unwrap();
-                    style.visuals.extreme_bg_color = egui::Color32::from_hex("#9b80cc").unwrap();
-                    style.visuals.code_bg_color = egui::Color32::from_hex("#ded3ee").unwrap();
-                    style.visuals.selection.bg_fill = egui::Color32::from_hex("#bda9dd").unwrap();
-                    style.visuals.hyperlink_color = egui::Color32::from_hex("#130496").unwrap();
-                    style.visuals.warn_fg_color = egui::Color32::from_hex("#1a087b").unwrap();
-                    style.visuals.error_fg_color = egui::Color32::from_hex("#1c0b62").unwrap();
-                    style.visuals.widgets.active.fg_stroke.color =
-                        egui::Color32::from_hex("#170b32").unwrap();
-                    style.visuals.override_text_color =
-                        Some(egui::Color32::from_hex("#12051d").unwrap());
-                });
+                cc.egui_ctx.set_fonts(gorbie_fonts());
+                cc.egui_ctx.set_style_of(egui::Theme::Light, gorbie_theme_light());
+                cc.egui_ctx.set_style_of(egui::Theme::Dark, gorbie_theme_dark());
 
                 Ok(Box::new(self))
             }),
